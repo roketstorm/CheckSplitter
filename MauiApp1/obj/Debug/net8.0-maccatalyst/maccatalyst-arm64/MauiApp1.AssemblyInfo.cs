469817923ebb577f4b01a0696101279b938ac591
roketstorm <a.shamsutdinov@icloud.com>
2024-07-18 18:56:46 +0500

    Replaced .gitignore

--- MauiApp1/obj/Debug/net8.0-maccatalyst/maccatalyst-arm64/MauiApp1.AssemblyInfo.cs
+++ MauiApp1/obj/Debug/net8.0-maccatalyst/maccatalyst-arm64/MauiApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+ba4d0d952a69802775f34095bc07657aadbcb5c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
