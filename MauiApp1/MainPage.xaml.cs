0476216c2a39789c30e28fc04f45a1d0b94209ad
roketstorm <a.shamsutdinov@icloud.com>
2024-07-24 17:35:44 +0500

    Added new fields and business logic

--- MauiApp1/MainPage.xaml.cs
+++ MauiApp1/MainPage.xaml.cs
@@ -1,10 +1,32 @@
-﻿namespace MauiApp1;
+﻿using MauiApp1.ViewModels;
+
+namespace MauiApp1;
 
 public partial class MainPage : ContentPage
 {
+	readonly CheckViewModel vm;
+
 	public MainPage()
 	{
 		InitializeComponent();
+		vm = (CheckViewModel)BindingContext;
+
+		for (int i = 1; i < 16; i++)
+		{
+			NumberOfPeople.Items.Add(i.ToString());
+		}
+
+		NumberOfPeople.SelectedIndex = 0;
+	}
+
+	private void NumberOfPeople_SelectedIndexChanged(object sender, EventArgs e)
+	{
+		var picker = (Picker)sender;
+		int selectedIndex = picker.SelectedIndex;
+
+		if (selectedIndex != -1) {
+			vm.NumPeople = selectedIndex + 1;
+		}
 	}
 }
 
